dd454dfebd0161cafef1c5a04b0851d20eb08c18
Vadim-Vatrya <VatryaVV@gmail.com>
2021-06-16 01:25:05 +0300

    icon

---
+++ js/game.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById("game");
+const ctx = canvas.getContext("2d");
+
+const ground = new Image();
+ground.src = "./img/ground.png";
+
+const food = new Image();
+ground.src = "./img/food.png";
