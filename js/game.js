8bc2fd7c624ad4e26605a680e716867281f492c7
Vadim-Vatrya <VatryaVV@gmail.com>
2021-06-16 23:22:56 +0300

    come snake

--- js/game.js
+++ js/game.js
@@ -1,11 +1,11 @@
-const canvas = document.getElementById("game");
-const ctx = canvas.getContext("2d");
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
 
 const ground = new Image();
-ground.src = "./img/ground.png";
+ground.src = './img/ground.png';
 
 const foodImg = new Image();
-foodImg.src = "./img/foodImg.png";
+foodImg.src = './img/food.png';
 
 let box = 32;
 let score = 0;
@@ -21,45 +21,95 @@ snake[0] = {
   y: 10 * box,
 };
 
-document.addEventListener("keydown", direction);
-
 let dir;
+const direction = e => {
+  if (e.keyCode === 37 && dir !== 'right') dir = 'left';
+  else if (e.keyCode === 38 && dir !== 'down') dir = 'up';
+  else if (e.keyCode === 39 && dir !== 'left') dir = 'right';
+  else if (e.keyCode === 40 && dir !== 'up') dir = 'down';
+};
+document.addEventListener('keydown', direction);
 
-const direction = (event) => {
-  if (e.keyCode === 37 && dir !== "right") dir = "left";
-  else if (e.keyCode === 38 && dir !== "down") dir = "up";
-  else if (e.keyCode === 39 && dir !== "left") dir = "right";
-  else if (e.keyCode === 40 && dir !== "up") dir = "down";
+class Sound {
+  constructor(src) {
+    this.sound = document.createElement('audio');
+    this.sound.src = src;
+    this.sound.setAttribute('preload', 'auto');
+    this.sound.setAttribute('controls', 'none');
+    this.sound.style.display = 'none';
+  }
+  play() {
+    document.body.appendChild(this.sound);
+    this.sound.play();
+  }
+  stop() {
+    this.sound.pause();
+  }
+}
+
+const eatSound = new Sound('./audio/EatSound.ogg');
+const dieSound = new Sound('./audio/DieSound.ogg');
+
+const forceReload = () => {
+  dieSound.play();
+  clearInterval(game);
+  alert('Game Over! Score: ' + score);
+  dieSound.stop();
+  location.reload();
+};
+
+const eatTail = (head, arr) => {
+  arr.forEach(el => head.x === el.x && head.y === el.y && forceReload());
 };
 
 const drawGame = () => {
   ctx.drawImage(ground, 0, 0);
   ctx.drawImage(foodImg, food.x, food.y);
+
   for (let i = 0; i < snake.length; i += 1) {
-    ctx.fillStyle = i === 0 ? "green" : "red";
+    ctx.fillStyle = i === 0 ? 'green' : 'red';
     ctx.fillRect(snake[i].x, snake[i].y, box, box);
   }
 
-  ctx.fillStyle = "white";
-  ctx.font = "50px Arial";
+  ctx.fillStyle = 'white';
+  ctx.font = '50px Arial';
   ctx.fillText(`score: ${score}`, box * 6, box * 1.6);
 
   let snakeX = snake[0].x;
   let snakeY = snake[0].y;
 
-  snake.pop();
+  if (
+    snakeX < box ||
+    snakeX > box * 17 ||
+    snakeY < 3 * box ||
+    snakeY > box * 17
+  )
+    forceReload();
 
-  if (dir === "left") snakeX -= box;
-  if (dir === "right") snakeX += box;
-  if (dir === "up") snakeY -= box;
-  if (dir === "down") snakeY += box;
+  if (snakeX === food.x && snakeY === food.y) {
+    eatSound.play();
+    score += 1;
+    food = {
+      x: Math.floor(Math.random() * 17 + 1) * box,
+      y: Math.floor(Math.random() * 15 + 3) * box,
+    };
+  } else {
+    snake.pop();
+  }
+
+  if (dir === 'left') snakeX -= box;
+  if (dir === 'right') snakeX += box;
+  if (dir === 'up') snakeY -= box;
+  if (dir === 'down') snakeY += box;
 
   const newHead = {
     x: snakeX,
     y: snakeY,
   };
 
+  eatTail(newHead, snake);
+
   snake.unshift(newHead);
 };
 
-let game = setInterval(drawGame, 100);
+const game = setInterval(drawGame, 100);
